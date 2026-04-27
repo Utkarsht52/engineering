0a7d62d9a4c580b89bd33b147c406b217113b5dc
Utkarsh Tiwari <81401599+Utkarsht52@users.noreply.github.com>
2026-04-27 22:32:40 +0530

    Update CodeToReview.cs
    People should probably be named Person.
    GetPeople(int i) parameter i should have a meaningful name like count.
    XML comments are not useful right now; they repeat method names instead of explaining behavior.
    _people can be readonly.
    GetPeople appends to the same internal list every time, so repeated calls keep growing old data.
    GetMarried has business-rule smell: "test" check looks hardcoded and unclear.
    DateTime.Now and DateTime.UtcNow are mixed; choose one, preferably UTC.

--- CodeToReview.cs
+++ CodeToReview.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+// First Issue: It should be using System.Collegctions.Generic;
 using System.Collegctions.Generic;
 using System.Linq;
 
 namespace Utility.Valocity.ProfileHelper
 {
+    // It can be Person 
     public class People
     {
+        // here it should be  private static readonly DateTimeOffset Under16 = DateTimeOffset.UtcNow.AddYears(-16) or name can be Under15
      private static readonly DateTimeOffset Under16 = DateTimeOffset.UtcNow.AddYears(-15);
      public string Name { get; private set; }
      public DateTimeOffset DOB { get; private set; }
@@ -34,13 +37,16 @@ namespace Utility.Valocity.ProfileHelper
         /// <returns>List<object></returns>
         public List<People> GetPeople(int i)
         {
+            // i can be count
             for (int j = 0; j < i; j++)
             {
                 try
                 {
                     // Creates a dandon Name
                     string name = string.Empty;
+                    // It can be created outside
                     var random = new Random();
+                    // It will always be zero so, else case can be removed, because in random first number is inclusive, second one is exclusive
                     if (random.Next(0, 1) == 0) {
                         name = "Bob";
                     }
@@ -48,11 +54,12 @@ namespace Utility.Valocity.ProfileHelper
                         name = "Betty";
                     }
                     // Adds new people to the list
+                    // 356 should be 365 also here leap year is ignored
                     _people.Add(new People(name, DateTime.UtcNow.Subtract(new TimeSpan(random.Next(18, 85) * 356, 0, 0, 0))));
                 }
                 catch (Exception e)
                 {
-                    // Dont think this should ever happen
+                    // Dont think this should ever happen, also here we should throw exception in case if any for more clarification or log in file
                     throw new Exception("Something failed in user creation");
                 }
             }
@@ -61,6 +68,7 @@ namespace Utility.Valocity.ProfileHelper
 
         private IEnumerable<People> GetBobs(bool olderThan30)
         {
+            // here again 365 should be there, also x.DOB <= DateTime.Now.Subtract(new TimeSpan(30 * 365, 0, 0, 0))
             return olderThan30 ? _people.Where(x => x.Name == "Bob" && x.DOB >= DateTime.Now.Subtract(new TimeSpan(30 * 356, 0, 0, 0))) : _people.Where(x => x.Name == "Bob");
         }
 
@@ -68,6 +76,7 @@ namespace Utility.Valocity.ProfileHelper
         {
             if (lastName.Contains("test"))
                 return p.Name;
+            // this code can be removed since substring data is not assigned to some variable
             if ((p.Name.Length + lastName).Length > 255)
             {
                 (p.Name + " " + lastName).Substring(0, 255);
